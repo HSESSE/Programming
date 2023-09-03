ddfd65015597345fed04df716aaf608c46cb29ce
Alex Glushko <aglushko@hse.ru>
2023-09-03 20:11:47 +0300

    Add demo for first seminar

---
+++ Seminars/Seminar01/Demo/InputOutput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Any input:");
+        string userInput = Console.ReadLine();
+        Console.WriteLine(userInput);
+
+        Console.WriteLine("Double input:");
+        string str = Console.ReadLine();
+        double r = double.Parse(str);
+        Console.WriteLine(r.ToString());
+        Console.WriteLine("Good luck with your studies!");
+    }
+}
