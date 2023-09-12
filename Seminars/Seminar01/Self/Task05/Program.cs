b43257e836b4453b2628613d93c84bb6c07b778b
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2023-09-12 20:27:56 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,9 @@
 ﻿// Your code here!
+class Program {
+    static void Main() {
+        double voltage = double.Parse(Console.ReadLine());
+        double resistance = double.Parse(Console.ReadLine());
+        Console.WriteLine("1. I = {0}", voltage/resistance);
+        Console.WriteLine("2. P = {0}", Math.Pow(voltage, 2)/resistance);
+    }
+}
