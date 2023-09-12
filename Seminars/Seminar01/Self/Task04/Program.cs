e2017cb81476795d78a596b5e6c0d8964fd90475
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2023-09-12 19:11:00 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,10 @@
 ﻿// Your code here!
+class Program {
+    static void Main() {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("Напишите свое имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, " + name);
+    }
+}
