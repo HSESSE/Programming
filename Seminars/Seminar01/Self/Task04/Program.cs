e96d5d50476eecd756c60261651ee0dc411667ac
Moge Lolya <mogemoge9111@gmail.com>
2023-09-12 22:09:50 +0300

    Замотаева Анастасия 239-2

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,18 @@
-﻿// Your code here!
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            string umya = Console.ReadLine();
+
+            Console.WriteLine("Здравствуйте, " + umya);
+
+
+        }
+    }
+}
