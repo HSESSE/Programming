507cec55a11d08799719d4788356df1505f21c03
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2023-09-12 18:39:17 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+class Program {
+    static void Main() {
+        string surname = Console.ReadLine();
+        string name = Console.ReadLine();
+        string fathersName = Console.ReadLine();
+        Console.WriteLine("Фамилия: " + surname);
+        Console.WriteLine("Имя: " + surname);
+        Console.WriteLine("Отчество: " + fathersName);
+    }
+}
