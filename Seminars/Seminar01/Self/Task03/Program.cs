41cb02cc1362a879d205c67839d63d130a2172d7
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2023-09-12 18:46:54 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,8 @@
 ﻿// Your code here!
+class Program {
+    static void Main() {
+        Console.Write("Напишите свое имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, " + name);
+    }
+}
