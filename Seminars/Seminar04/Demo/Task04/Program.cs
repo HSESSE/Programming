a8edc4d8656b9c361de11585024395ef30aa5881
leonchik <leon.ru123@mail.ru>
2023-09-13 16:48:32 +0300

    add 4th seminar

---
+++ Seminars/Seminar04/Demo/Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿uint level = 0;
+string output = level switch
+{
+
+    >= 1 and <= 4 => "Начальная школа",
+    >= 5 and <= 9 => "Средняя школа",
+    >= 10 and <= 11 => "Старшая школа",
+    _ => ("Нет такого класса в школе")
+};
+Console.WriteLine(output);
