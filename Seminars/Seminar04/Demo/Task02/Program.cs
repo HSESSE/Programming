83738ae3dbef0976493b4720e74132cafdd5f68a
leonchik <leon.ru123@mail.ru>
2023-09-14 08:38:11 +0300

    fix codes

--- Seminars/Seminar04/Demo/Task02/Program.cs
+++ Seminars/Seminar04/Demo/Task02/Program.cs
@@ -7,15 +7,20 @@ switch (shool)
     case 2:
     case 3:
     case 4:
-        Console.WriteLine("Начальная школа"); break;
+        Console.WriteLine("Начальная школа");
+        break;
     case 5:
     case 6:
     case 7:
     case 8:
     case 9:
-        Console.WriteLine("Средняя школа"); break;
+        Console.WriteLine("Средняя школа");
+        break;
     case 10:
     case 11:
-        Console.WriteLine("Старшая школа"); break;
-    default: Console.WriteLine("Такого класса в школе нет!"); break;
+        Console.WriteLine("Старшая школа");
+        break;
+    default:
+        Console.WriteLine("Такого класса в школе нет!");
+        break;
 }
