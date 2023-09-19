984f6b1aa0455dd219f3f592ecb638f5e630d41c
Алексей Чёрный <avea05.alex@yandex.ru>
2023-09-19 18:33:25 +0300

    task01

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        double.TryParse(Console.ReadLine(), out double l);
+
+        if (1 <= n && n < 11 && n != 2 && l > 0 && Math.Tan(Math.PI / n) > 0)
+        {
+            double s = ((n * l) * (n * l)) / (4 * Math.Tan(Math.PI / n));
+            Console.WriteLine(s);
+        }
+    }
+}
