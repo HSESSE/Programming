225aad71a6cb80fd16eb42f248844a0cc2c286c7
Алексей Чёрный <avea05.alex@yandex.ru>
2023-09-19 18:08:56 +0300

    task03

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+using System;
+class Program
+{
+    static void Main()
+    {
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine($"Привет, {firstname}!");
+    }
+}
