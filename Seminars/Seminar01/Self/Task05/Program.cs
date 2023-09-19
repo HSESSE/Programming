dbd1dca67c7e2321ad193b1a09b1043709b68fd9
Алексей Чёрный <avea05.alex@yandex.ru>
2023-09-19 18:10:06 +0300

    task05

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Your code here!
+using System;
+class Program
+{
+    static void Main()
+    {
+        float u = Convert.ToInt32(Console.ReadLine());
+        float r = Convert.ToInt32(Console.ReadLine());
+        float i = u / r;
+        float p = (u * u) / r;
+        Console.WriteLine("I = {0}", i);
+        Console.WriteLine("P = {0}", p);
+    }
+}
