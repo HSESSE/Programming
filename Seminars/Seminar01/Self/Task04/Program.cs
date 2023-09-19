4ad140b47bf4cd51940c9615e6392c5a5b1d5516
Алексей Чёрный <avea05.alex@yandex.ru>
2023-09-19 18:09:37 +0300

    task04

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("floppABOBA");
+    }
+}
