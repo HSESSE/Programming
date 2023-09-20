c74df3c313167de951f6d5121abe08d737ed3361
qoxrdw <egorskorik398@gmail.com>
2023-09-20 03:46:00 +0300

    commit 4 sem

---
+++ Seminars/Seminar04/Self/Task04/task4/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A + оператор + B: ");
+            string? str = Console.ReadLine().Replace('.', ',');
+            Console.WriteLine();
+
+            string[] arr = str.Split();
+
+            double.TryParse(arr[0], out double A);
+            char.TryParse(arr[1], out char op);
+            double.TryParse(arr[2], out double B);
+
+            switch (op)
+            {
+                case ('+'):
+                    Console.WriteLine(A + B);
+                    break;
+
+                case ('-'):
+                    Console.WriteLine(A - B);
+                    break;
+
+                case ('*'):
+                    Console.WriteLine(A * B);
+                    break;
+
+                case ('/'):
+                    if (B != 0)
+                    {
+                        Console.WriteLine(A / B);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нельзя делить на ноль");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Что-то пошло не так");
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
