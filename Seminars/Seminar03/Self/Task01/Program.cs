133d7f1a2ea31ef2ca3d8a8b13affca0bc7c16e1
SubjectSp1rit <dividedwestvnd@yandex.ru>
2023-09-21 19:31:41 +0300

    seminar03 completed

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class program
+{
+    public static void Main()
+    {
+        Console.Write("Введите количество углов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите длину стороны: ");
+        float l = Convert.ToSingle(Console.ReadLine());
+
+        Console.Write($"Площадь фигуры равна: {(n * Math.Pow(l, 2)) / (4 * Math.Tan(Math.PI / n))}");
+
+        Console.ReadKey();
+    }
+}
---
+++ Seminars/Seminar04/Self/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        string? a = Console.ReadLine();
+        char b = a[0];
+        int c = Convert.ToInt32(b);
+
+        int F = c switch
+        {
+            >= 48 and <= 57 => 100,
+            >= 65 and <= 90 => 200,
+            >= 97 and <= 122 => 300,
+            _ => 400
+        };
+        
+        Console.WriteLine(F);
+        Console.ReadKey();
+    }
+}
