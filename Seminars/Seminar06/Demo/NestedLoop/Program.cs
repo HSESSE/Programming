747d8a24eee2d4d70d0a3b3503df246a7053a7d8
Nikita Kitaev <nik_kitaev@mail.ru>
2023-09-22 17:23:14 +0300

    Seminar06 Demo

---
+++ Seminars/Seminar06/Demo/NestedLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+int b;
+int c;
+int sum = 0;
+
+for (a = 1; a < 3; a++)
+{
+    for (b = 1; b < 3; b++)
+    {
+        for (c = 1; c < 3; c++) //Самый быстрый цикл.
+        {
+            sum += a + b + c;
+            Console.Write($" a = {a} b = {b} c = {c} sum = {sum}\n");
+        }
+    }
+}
+
+Console.ReadLine();
