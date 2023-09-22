75678ad091399b29c28efd1c619388c84452c0c7
Nikita Kitaev <nik_kitaev@mail.ru>
2023-09-22 17:31:33 +0300

    Seminar06 ToDo dded

---
+++ Seminars/Seminar06/ToDo/ToDo01/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+int b;
+int c;
+int sum = 0;
+
+for (a = 1; a < 3; a++)
+{
+    for (b = 1; b < 3; b++)
+    {
+        for (c = 1; c < 3; c++)
+        {
+            sum += a + b + c;
+            Console.Write($" a = {a} b = {b} c = {c} sum = {sum}\n");
+        }
+    }
+}
+Console.ReadLine();
