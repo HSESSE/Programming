85be01d32410fec323cb2a8da5def4e0fce92bb4
Nikita Kitaev <nik_kitaev@mail.ru>
2023-09-24 17:48:54 +0300

    SepcifiedSum codestyle fixed

--- Seminars/Seminar05/Demo/SpecifiedSum/Program.cs
+++ Seminars/Seminar05/Demo/SpecifiedSum/Program.cs
@@ -8,7 +8,9 @@ int summ = 0; // Сумму обнуляем до цикла.
 do
 {
     if (number % 3 == 0 && (number) % 7 == 0)
+    {
         summ += number;
+    }
 } while (++number < 100);
 
 Console.WriteLine(summ); // 210
