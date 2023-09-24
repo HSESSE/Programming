4d0fc39ea4a6f79af3652f3b58c347027876f718
Nikita Kitaev <nik_kitaev@mail.ru>
2023-09-24 16:37:58 +0300

    configuration fixed

--- Seminars/Seminar05/Demo/ParametricFor/Program.cs
+++ Seminars/Seminar05/Demo/ParametricFor/Program.cs
@@ -2,8 +2,16 @@
  * Demo 03. Параметрический цикл for.
  */
 
-int n = 10; for (; ; )
+int n = 10;
+
+for (; ; )
 {
-    Console.WriteLine(n); if (n >= 15) break;
+    Console.WriteLine(n);
+
+    if (n >= 15)
+    {
+        break;
+    }
+
     n += 2;
 }
--- Seminars/Seminar05/Demo/SpecifiedSum/Program.cs
+++ Seminars/Seminar05/Demo/SpecifiedSum/Program.cs
@@ -4,8 +4,11 @@
 
 int number = 1;
 int summ = 0; // Сумму обнуляем до цикла.
+
 do
 {
     if (number % 3 == 0 && (number) % 7 == 0)
         summ += number;
-} while (++number < 100) ; Console.WriteLine(summ); // 210
+} while (++number < 100);
+
+Console.WriteLine(summ); // 210
