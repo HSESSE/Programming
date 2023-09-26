60733951f75ecc58022b6314f2c0ec5f9f43b5bf
Moge Lolya <mogemoge9111@gmail.com>
2023-09-26 21:49:08 +0300

    Seminar05 Task01-09

---
+++ Seminars/Seminar05/Self/Task02/Task02/Task02/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vvod;
+            int kolvoint = 0;
+            int kolvodouble = 0;
+            int kolvo = 0;
+
+
+            do
+            {
+                vvod = Console.ReadLine();
+
+
+                if (int.TryParse(vvod, out int i))
+                {
+                    kolvoint++;
+                }
+                else if (double.TryParse(vvod, out double a))
+                {
+                    kolvodouble++;
+                }
+                else
+                {
+                    kolvo++;
+                }
+
+
+            } while (vvod != "0");
+            
+
+            Console.WriteLine($"{kolvoint}, {kolvodouble}, {kolvo}");
+        }
+    }
+}
---
+++ Seminars/Seminar05/Self/Task06/Task06/Task06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 1000 ; i++)
+            {
+                if (i > 99)
+                {
+                    int a3 = i % 10;
+                    int a2 = i / 10 % 10;
+                    int a1 = i / 100;
+                    if (a3 == a2 && a2 == a1 && a3 == a1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
