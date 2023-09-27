8d5683a79df52d84e5c43b19e4e2ecc27eb2edb3
Алексей Чёрный <avea05.alex@yandex.ru>
2023-09-27 16:51:47 +0300

    self01

--- Seminars/Seminar06/Self/Self01/Program.cs
+++ Seminars/Seminar06/Self/Self01/Program.cs
@@ -1,1 +1,19 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        bool p = false;
+        bool q = false;
+        do
+        {
+            do
+            {
+                bool F = !(p & q) & !(p | !q);
+                Console.WriteLine($"p = {p} q = {q} F = {F}");
+                q = !q;
+            } while (q);
+            p = !p;
+        } while (p);
+    }
+}
