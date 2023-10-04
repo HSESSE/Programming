f5aef5911c5e25cccd6a7a388c138adf2e2dba9a
Алексей Чёрный <avea05.alex@yandex.ru>
2023-10-04 18:02:44 +0300

    self09

--- Seminars/Seminar05/Self/Self09/Program.cs
+++ Seminars/Seminar05/Self/Self09/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Ваше решение.
+using System;
+class Program
+{
+    public static void Main()
+    {
+        double.TryParse(Console.ReadLine(), out double x);
+        double.TryParse(Console.ReadLine(), out double y);
+        
+        if (((x * x + y * y <= 4) && (y <= 0.5 * x) && (y >= 0)) || ((x * x + y * y <= 4) && (y <= 0) && (x >= 0)))
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
