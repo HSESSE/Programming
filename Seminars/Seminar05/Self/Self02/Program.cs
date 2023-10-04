9ddbc485ddf2e9e491b06d1304797a9f3dc2129b
Алексей Чёрный <avea05.alex@yandex.ru>
2023-10-04 12:17:33 +0300

    self02

--- Seminars/Seminar05/Self/Self02/Program.cs
+++ Seminars/Seminar05/Self/Self02/Program.cs
@@ -1,1 +1,34 @@
-﻿// Ваше решение.
+﻿using System;
+// Ваше решение.
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count_int = 0;
+        int count_double = 0;
+        int count_oth = 0;
+
+        string s = Console.ReadLine();
+        do
+        {
+            int.TryParse(s, out int a);
+            double.TryParse(s, out double b);
+            if (a != 0  || s == "0")
+            {
+                count_int += 1;
+            }
+            else if (b != 0)
+            {
+                count_double += 1;
+            }
+            else
+            {
+                count_oth += 1;
+            }
+            s = Console.ReadLine();
+        } while (s != "0");
+        Console.WriteLine($"Количество int: {count_int}");
+        Console.WriteLine($"Количество double: {count_double}");
+        Console.WriteLine($"Количество нечисловых: {count_oth}");
+    }
+}
