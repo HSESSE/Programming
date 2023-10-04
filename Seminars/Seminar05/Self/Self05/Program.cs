1ba37840b5ccc2c870a1352100834050674fca5a
Алексей Чёрный <avea05.alex@yandex.ru>
2023-10-04 16:36:56 +0300

    self05

--- Seminars/Seminar05/Self/Self05/Program.cs
+++ Seminars/Seminar05/Self/Self05/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Ваше решение.
+using System;
+class Program
+{
+    public static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out int k);
+        int k1 = 1;
+        double summa = 0;
+        while (k1 <= k)
+        {
+            summa += 1.0 / k1;
+            k1 += 1;
+        }
+        string s = string.Format("{0:F4}", summa);
+        Console.WriteLine(s);
+        
+    }
+}
