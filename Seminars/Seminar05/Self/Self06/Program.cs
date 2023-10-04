f33f5bb6172bc06a1395ed03d1122521d464b3d4
Алексей Чёрный <avea05.alex@yandex.ru>
2023-10-04 16:44:43 +0300

    self06

--- Seminars/Seminar05/Self/Self06/Program.cs
+++ Seminars/Seminar05/Self/Self06/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Ваше решение.
+using System;
+class Program
+{
+    public static void Main()
+    {
+        int n = 111;
+        while (n <= 999)
+        {
+            Console.WriteLine(n);
+            n += 111;
+        }
+    }
+}
